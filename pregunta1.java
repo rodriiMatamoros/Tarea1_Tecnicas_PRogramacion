f147dc360b4f03c7ae802e8ec42a9419abef0141
rodriii <113851706+rodriiMatamoros@users.noreply.github.com>
2023-02-02 20:30:27 +0000

    comit_1

---
+++ pregunta1.java
@@ -0,0 +1,21 @@
+//¿Qué algoritmos usas en tu vida cotidiana? 
+
+
+
+// Path: pregunta2.java
+// Compare this snippet from pregunta3.java:
+// //¿Haz hecho alguna vez un algoritmo para un programa? ¿Qué hacía? ¿Era correcto y eficiente?
+//¿Qué algoritmos usas en tu vida cotidiana?
+//¿Qué algoritmos crees que usan por tus juegos y aplicaciones favoritas?
+
+ 
+import java.util.Scanner;
+
+public class pregunta1 {    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¿Qué es un algoritmo?");
+        String respuesta = sc.nextLine();
+        System.out.println("¿Qué es un algoritmo? " + respuesta);
+    }
+}
